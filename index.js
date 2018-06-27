cc8b454aa0ab5646ce9ff0c39536344e27b7026b
Jeremiah Volkmer <jvolkmer32@gmail.com>
2018-06-27 13:22:26 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var http = require("http"); 
+var fs = require('fs');
+
+http.createServer(function(req,res) {
+  var path = req.url.toLowerCase();
+  switch(path) {
+    case '/':
+    fs.readFile('home.html', function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+        });
+      break;
+    case '/about':
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('About page');
+      break;
+    default:
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.end('Not found');
+      break;
+    }
+}).listen(process.env.PORT || 3000);
