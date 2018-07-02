2c82bf0b306393e7660b0d92a235d14832397948
Jeremiah Volkmer <jvolkmer32@gmail.com>
2018-07-02 14:55:52 -0700

    hw1

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 var http = require("http"); 
 var fs = require('fs');
-var mod = require('./mod.js')
+var mod = require('./mod.js');
+
+
 
 http.createServer(function(req,res) {
   var path = req.url.toLowerCase();
 
   switch(path) {
-    case '/':
+    case '/getall':
     fs.readFile('mod.js', function(err, data) {
         res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
+        res.write(mod.getAll(surfboards));
         res.end();
         });
       break;
@@ -17,6 +19,10 @@ http.createServer(function(req,res) {
       res.writeHead(200, {'Content-Type': 'text/plain'});
       res.end('About page');
       break;
+    case '/':
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Home page');
+    break;  
     default:
       res.writeHead(404, {'Content-Type': 'text/plain'});
       res.end('Not found');
