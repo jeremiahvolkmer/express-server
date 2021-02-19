1df5ae740001cef10b58c9c9281b5c4128645ad1
Jeremiah Volkmer <35974119+jeremiahvolkmer@users.noreply.github.com>
2021-02-19 21:01:03 +0000

    Delete testdb.js

--- testdb.js
+++
@@ -1,8 +0,0 @@
-var Surfboard = require("./models/mod2");
-
-// return all records
-Surfboard.find({}, (err, items) => {
-  if (err) return next(err);
-  console.log(items.length);
-  // other code here
-});
